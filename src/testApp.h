f22099b0ac45a26a5d5932d1d565160de8160d09
Daniel T <tsadok@bigfoot.com>
2011-05-02 23:06:24 -0400

    started with VBO example and rewrote

--- src/testApp.h
+++ src/testApp.h
@@ -6,10 +6,6 @@
 #include "ofMain.h"
 #include "ofxAndroid.h"
 
-#define GRID_WIDTH  16
-#define GRID_HEIGHT 16
-#define LENGTH		2 * 8
-
 class testApp : public ofxAndroidApp {
 	
 public:
@@ -28,14 +24,6 @@ public:
 	void touchDoubleTap(ofTouchEventArgs &touch);
 	void touchCancelled(ofTouchEventArgs &touch);
 	
-	ofVbo	vbo;
-	ofVec3f pos[GRID_WIDTH*GRID_HEIGHT*LENGTH];
-	ofVec3f center;
-
-	float   restLength;
-	int		total;
-	int		space;
-	int		count;
 	bool	bPause;
 	int		zoom, zoomTarget;
 
